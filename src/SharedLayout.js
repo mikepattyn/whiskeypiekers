cde27a082ff66cf8b00a283b5fdecc103d674747
Mike Pattyn <mike@givtapp.net>
2019-04-05 08:14:58 +0200

    created a SharedLayout component for the App component to be loaded in

---
+++ src/SharedLayout.js
@@ -0,0 +1,14 @@
+import React, { Component} from "react";
+import {hot} from "react-hot-loader";
+
+class SharedLayout extends Component {
+    render(){
+        return(
+            <div class='col-md-12' id="mainLayout">
+
+            </div>
+        )
+    }
+}
+
+export default hot(module)(SharedLayout);
