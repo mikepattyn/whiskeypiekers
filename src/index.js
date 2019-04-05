0eb764b522dce965f1d5506ba64c27d08d43ff96
Mike Pattyn <mike@givtapp.net>
2019-04-05 08:15:52 +0200

    Rendering the SharedLayout component to root and render the App component in SharedLayout

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+// Start of component imports
 import App from "./App.js";
-ReactDOM.render(<App />, document.getElementById("root"));
+import SharedLayout from "./SharedLayout.js";
+// End of component imports
+
+ReactDOM.render(<SharedLayout />, document.getElementById("root")); // Add a shared layout to root
+ReactDOM.render(<App />, document.getElementById("mainLayout")); // Add the app layout to shared layout
