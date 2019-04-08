0027c444e3ddc190e7c86f8a1ee59412423a3753
Mike Pattyn <mike@givtapp.net>
2019-04-08 14:57:37 +0200

    Revert "Added SectionOne component with a table and data for the table"
    This reverts commit 81e25e609adf0faf175c4846a8602391dbf6221f.

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -4,9 +4,6 @@ import { hot } from "react-hot-loader/root"
 export interface AppProps { compiler: string; framework: string; programmer: string; }
 
 const App = (props: AppProps) => 
-    <>
-        <h1 className="text-center">Hello from {props.compiler}, {props.framework} and {props.programmer}!</h1>
-        <div id="section-one"></div>
-    </>
-
+    <h1>Hello from {props.compiler}, {props.framework} and {props.programmer}!</h1>;
+    
 export default hot(App)
