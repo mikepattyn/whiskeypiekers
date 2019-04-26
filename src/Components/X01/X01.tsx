1b26708765b892ce0541114714debbd66e0dd863
Mike Pattyn <mike@givtapp.net>
2019-04-26 20:45:54 +0200

    using the x01propsforselects object to pass it to the state and pass the lists seperatly in to the props of the x01trainingform

--- src/Components/X01/X01.tsx
+++ src/Components/X01/X01.tsx
@@ -4,23 +4,23 @@ import X01TrainingForm from './X01TrainingForm'
 import { fetchListData } from '../Actions/x01Actions'
 import { connect } from 'react-redux';
 import { maxScoresList, checkOutList, levelList, setList, legsList } from '../../Static/X01';
+import { X01PropsForSelects } from '../Classes/X01';
+import { ListWithDataArrays } from '../../Interfaces/General';
 
-class X01 extends React.Component<any, {}> {
+class X01 extends React.Component<any, any> {
     componentWillMount(){
-        const listData = {
-            "scores": maxScoresList.map((number: number, index: number) => { return (<option key={index} value={number}>{number}</option>) }) || null,
-            "checkouts": checkOutList.map((number: string, index: number) => { return (<option key={index} value={number}>{number}</option>) }) || null,
-            "levels": levelList.map((level: number) => { return (<option key={level + 1} value={level + 1}>Level {level + 1}</option>) }) || null,
-            "sets": setList.map((set: number) => { return (<option key={set + 1} value={set + 1}>{set + 1}</option>) }) || null,
-            "legs": legsList.map((leg: number) => { return (<option key={leg + 1} value={leg + 1}>{leg + 1}</option>) }) || null,
-        }
-        this.setState({listData})
+        const list: X01PropsForSelects = new X01PropsForSelects()
+        list.data = new ListWithDataArrays()
+        list.data.scores = maxScoresList.map((number: number, index: number) => { return (<option key={index} value={number}>{number}</option>) })
+        list.data.checkouts = checkOutList.map((number: string, index: number) => { return (<option key={index} value={number}>{number}</option>) }) || null,
+        list.data.levels = levelList.map((level: number) => { return (<option key={level + 1} value={level + 1}>Level {level + 1}</option>) }) || null,
+        list.data.sets = setList.map((set: number) => { return (<option key={set + 1} value={set + 1}>{set + 1}</option>) }) || null,
+        list.data.legs = legsList.map((leg: number) => { return (<option key={leg + 1} value={leg + 1}>{leg + 1}</option>) }) || null,
+        
+        this.setState(list)
     }
     constructor(props: any){
         super(props)
-        this.state = {
-            listData: []
-        }
     }
     render() {
         console.log(this.state)
@@ -28,7 +28,7 @@ class X01 extends React.Component<any, {}> {
         switch (step) {
             case 1:
                 return (
-                    <X01TrainingForm props={this.state} />
+                    <X01TrainingForm scores={this.state.data.scores} checkouts={this.state.data.checkouts} levels={this.state.data.levels} sets={this.state.data.sets} legs={this.state.data.legs} />
                 )
             default:
                 return <></>
