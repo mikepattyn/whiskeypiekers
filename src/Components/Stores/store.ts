6a1355d0cd321c4c22968f92a874e220cb79ef17
Mike Pattyn <mike@givtapp.net>
2019-04-26 21:12:45 +0200

    remove devtools because it wasn't working

--- src/Components/Stores/store.ts
+++ src/Components/Stores/store.ts
@@ -3,14 +3,12 @@ import thunk from 'redux-thunk'
 import rootReducer from './../Reducers/index'
 const initialState = {}
 const middleware = [thunk]
-let devtools: any = (window as any)['devToolsExtension'] ? (window as any)['devToolsExtension']() : (f:any)=>f;
 
 const store = createStore(
     rootReducer, 
     initialState, 
     compose(
-        applyMiddleware(...middleware), 
-        devtools
+        applyMiddleware(...middleware)
     )
 )
 export default store
